aee24fa59d3962797dd28053707483c866d639a0
Mayaank Vadlamani <mayaankvad@gmail.com>
2018-05-06 11:00:52 -0400

    update

--- Map&Set/src/schoolClub/Student.java
+++ Map&Set/src/schoolClub/Student.java
@@ -1,5 +1,54 @@
 package schoolClub;
 
-public class Student {
+public class Student implements Comparable<Student> {
+
+	private String first;
+	private String last;
+	private String id;
+
+	
+	public Student(String first, String last, String id) {
+		this.first = first;
+		this.last = last;
+		this.id = id;
+	}
+	
+	
+	
+	
+	@Override
+	public int compareTo(Student o) {
+		if(this.last.equalsIgnoreCase(o.last)) {
+			return this.first.toLowerCase().compareTo(o.first.toLowerCase());
+		}
+		return this.last.toLowerCase().compareTo(o.last.toLowerCase());
+	}
+
+	
+	
+	
+	public String getFirst() {
+		return first;
+	}
+
+	public void setFirst(String first) {
+		this.first = first;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	public void setLast(String last) {
+		this.last = last;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 }
